a87fb112db0efbed5241b12fe43f3c1a43cc4cdb
hemantreddy <hemantreddy22@gmail.com>
2021-10-26 11:33:22 -0400

    merge conflicts

--- service/models.py
+++ service/models.py
@@ -13,6 +13,7 @@ name (string) - the name of the product
 quantity (int) - the quantity of the product
 
 """
+import flask
 import logging
 from enum import Enum
 from flask import Flask
@@ -33,8 +34,6 @@ class DataValidationError(Exception):
     pass
 
 class Inventory(db.Model):
-    
-    app:Flask = None
     
     # Inventory Schema
     
@@ -66,13 +65,14 @@ class Inventory(db.Model):
         if not self.id:
             raise DataValidationError("Update called with empty ID field")
         db.session.commit()
-
+    
     def delete(self):
         """Removes a product from the data store"""
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
-        
+
+
     def serialize(self) -> dict:
         """Serializes an Inventory into a dictionary"""
         return {

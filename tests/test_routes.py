fd05ba76d5690078c66d3f739ca6504c3e590c4f
Hemant Palavali <hemantp@Hemants-MacBook-Pro.local>
2021-12-08 21:16:24 -0500

    fixed tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -248,7 +248,6 @@ class TestInventoryServer(unittest.TestCase):
 		logging.debug(new_inv)
 		resp = self.app.delete(
 			BASE_URL + "/{}".format(new_inv["id"]),
-			content_type=CONTENT_TYPE_JSON,
 		)
 		self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 		self.assertEqual(len(resp.data), 0)

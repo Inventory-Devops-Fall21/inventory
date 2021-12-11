b39b46bc2d3dbf45badcce763b808c8023eaec39
YinWang3026 <yw2178@nyu.edu>
2021-12-11 14:38:07 -0500

    fixed handlers

--- service/error_handlers.py
+++ service/error_handlers.py
@@ -1,39 +1,57 @@
 from flask import jsonify
 from service.models import DataValidationError
-from . import app, status
+from service import app, status
+from service.routes import api
+from werkzeug.exceptions import NotFound, BadRequest
 
 ######################################################################
-# Error Handlers
+# Special API Registered Error Handlers
 ######################################################################
-@app.errorhandler(DataValidationError)
+
+# @api.errorhandler(DatabaseConnectionError)
+# def database_connection_error(error):
+#     """ Handles Database Errors from connection attempts """
+#     message = str(error)
+#     app.logger.critical(message)
+#     return {
+#         'status_code': status.HTTP_503_SERVICE_UNAVAILABLE,
+#         'error': 'Service Unavailable',
+#         'message': message
+#     }, status.HTTP_503_SERVICE_UNAVAILABLE
+
+@api.errorhandler(DataValidationError)
 def request_validation_error(error):
     """ Handles Value Errors from bad data """
     return bad_request(error)
 
-
-@app.errorhandler(status.HTTP_400_BAD_REQUEST)
+@api.errorhandler(BadRequest)
 def bad_request(error):
     """ Handles bad reuests with 400_BAD_REQUEST """
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
-        ),
-        status.HTTP_400_BAD_REQUEST,
-    )
+    return {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'error': 'Bad Request',
+        'message': message 
+    }, status.HTTP_400_BAD_REQUEST
+    
 
 
-@app.errorhandler(status.HTTP_404_NOT_FOUND)
+@api.errorhandler(NotFound)
 def not_found(error):
     """ Handles resources not found with 404_NOT_FOUND """
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
-        status.HTTP_404_NOT_FOUND,
-    )
+    return {
+        'status': status.HTTP_404_NOT_FOUND, 
+        'error': "Not Found", 
+        'message': message
+    }, status.HTTP_404_NOT_FOUND
 
+
+######################################################################
+# APP Registered Error Handlers
+######################################################################
 
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
 def method_not_supported(error):
--- service/routes.py
+++ service/routes.py
@@ -14,10 +14,10 @@ DELETE /inventory/{id} - deletes a inventory with a given id number
 # import sys
 # import logging
 # from typing_extensions import Required
-from flask import Flask, jsonify, request, url_for, make_response, abort
+# from flask import Flask, jsonify, request, url_for, make_response, abort
 from . import status, app  # HTTP Status Codes and Flask App
 from service.models import Inventory, Condition, DataValidationError, DatabaseConnectionError
-from werkzeug.exceptions import NotFound, BadRequest
+# from werkzeug.exceptions import NotFound, BadRequest
 
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
@@ -100,31 +100,6 @@ inv_args.add_argument('condition', type=str,
     required=False, help='List Inventory by condition')
 inv_args.add_argument('need_restock', type=inputs.boolean, 
     required=False, help='List Inventory by whether it needs restock')
-
-######################################################################
-# Special Error Handlers
-######################################################################
-@api.errorhandler(DataValidationError)
-def request_validation_error(error):
-    """ Handles Value Errors from bad data """
-    message = str(error)
-    app.logger.error(message)
-    return {
-        'status_code': status.HTTP_400_BAD_REQUEST,
-        'error': 'Bad Request',
-        'message': message
-    }, status.HTTP_400_BAD_REQUEST
-
-# @api.errorhandler(DatabaseConnectionError)
-# def database_connection_error(error):
-#     """ Handles Database Errors from connection attempts """
-#     message = str(error)
-#     app.logger.critical(message)
-#     return {
-#         'status_code': status.HTTP_503_SERVICE_UNAVAILABLE,
-#         'error': 'Service Unavailable',
-#         'message': message
-#     }, status.HTTP_503_SERVICE_UNAVAILABLE
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
